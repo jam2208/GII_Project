1cacaf84ea85fd53005909b86d72564753e3a104
jam2208 <junwoo.park82@icloud.com>
2021-08-12 09:23:00 +0900

    2021.08.12 Commit 1

--- commentapp/views.py
+++ commentapp/views.py
@@ -22,7 +22,6 @@ class CommentCreateView(CreateView):
         form.instance.article_id = self.request.POST.get('article_pk')
         return super().form_valid(form)
 
-
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
 
@@ -34,4 +33,4 @@ class CommentDeleteView(DeleteView):
     template_name = 'commentapp/delete.html'
 
     def get_success_url(self):
-        return reverse('articleapp:detail', kwargs={'pk':self.object.article.pk})
+        return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
