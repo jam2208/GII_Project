c1dc8950c4023009ad23ced9f4d444f185b5aa9c
jam2208 <junwoo.park82@icloud.com>
2021-08-30 09:52:08 +0900

    2021.08.30 Commit 3

--- decorators.py
+++ decorators.py
@@ -1,16 +1,3 @@
-#
-# def decorator(func):
-#     def decorated():
-#         print('함수 시작!')
-#         func()
-#         print('함수 끝!')
-#     return decorated
-#
-# @decorator
-# def hello_world():
-#     print('Hello World!')
-#
-# hello_world()
 
 def decorator(area):
     def decorated(w, h):
